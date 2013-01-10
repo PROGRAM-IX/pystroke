2ad4ba7753b87a1a486a80e815d4f77af5c10b27
James Heslin <program.ix.j@gmail.com>
2013-01-10 12:10:31 +0000

    PYSTROKE v0.07! Added a locals module which keeps track of various flags (at present just the state-switch/quit flags for Game and GameEngine)

---
+++ locals.py
@@ -0,0 +1,11 @@
+# Flags to pass between Game and GameEngine and control which GameEngine runs
+
+# Switch to next GameEngine
+SWITCH_FLAG = 0
+# Quit altogether
+QUIT_FLAG = 1
+# (By default) Do nothing
+CONTINUE_FLAG = 2
+
+
+
